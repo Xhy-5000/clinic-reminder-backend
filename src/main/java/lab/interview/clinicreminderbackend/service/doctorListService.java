26f3907f16192714a1c837824b2d8d30407a3a67
Xhy-5000 <118010339@link.cuhk.edu.cn>
2023-02-03 17:08:56 -0500

    fix: add sort func for doctor get reminder list

--- src/main/java/lab/interview/clinicreminderbackend/service/doctorListService.java
+++ src/main/java/lab/interview/clinicreminderbackend/service/doctorListService.java
@@ -8,8 +8,7 @@ import lab.interview.clinicreminderbackend.entity.reminder;
 import lab.interview.clinicreminderbackend.entity.result;
 import lab.interview.clinicreminderbackend.entity.patient;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class doctorListService {
@@ -18,24 +17,67 @@ public class doctorListService {
     @Autowired
     patientMapper patientMapper;
 
+    private class listItem{
+        int id;
+        String name;
+        int[] priority;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int[] getPriority() {
+            return priority;
+        }
+
+        public void setPriority(int[] priority) {
+            this.priority = priority;
+        }
+    }
+
     public result reminderList(int doctorid){
         //可以给patient加三个（高中低）属性来记录未完成reminder数量，每次新增或完成reminder进行修改
         //也可以增加一个表格patient-unifinished来记录
         patient[] patients = patientMapper.findByDoctorid(doctorid);
         String[] priorities = {"HIGH","MID","LOW"};
-        Map<Integer, Map<String, int[]>> obj = new HashMap<>();
+        List<listItem> obj = new ArrayList<>();
         for(patient patient:patients){
             int patientid = patient.getPatientid();
-            Map<String, int[]> sub = new HashMap<>();
             int[] countPriority = new int[3];
+            listItem listItem = new listItem();
             for(int i=0; i<3; i++){
                 String prioriy = priorities[i];
                 reminder[] reminders = reminderMapper.countByPriority(patientid,prioriy);
                 countPriority[i] = reminders.length;
             }
-            sub.put(patient.getPatientname(),countPriority);
-            obj.put(patientid, sub);
+            listItem.setId(patientid);
+            listItem.setName(patient.getPatientname());
+            listItem.setPriority(countPriority);
+            obj.add(listItem);
         }
+        Collections.sort(obj,(o1,o2)->{
+            int[] p1 = o1.getPriority();
+            int[] p2 = o2.getPriority();
+            if(p1[0]!=p2[0]){
+                return p2[0]-p1[0];
+            }else if(p1[1]!=p2[1]){
+                return p2[1]-p1[1];
+            }else{
+                return p2[2]-p1[2];
+            }
+        });
         result result = new result();
         result.setMsg("successfully get list");
         result.setObj(obj);
